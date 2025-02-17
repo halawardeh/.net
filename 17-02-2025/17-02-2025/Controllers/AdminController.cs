8459ff9d601107445b174c229d16adf6e1050ddf
halawardeh <halawardeh12102001@gmail.com>
2025-02-17 21:46:07 +0300

    17-02

---
+++ 17-02-2025/17-02-2025/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _17_02_2025.Controllers
+{
+    public class AdminController : Controller 
+    {
+        public IActionResult Admin()
+        {
+            return View(); 
+        }
+    }
+}
---
+++ 17-02-2025/17-02-2025/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+using System.Diagnostics;
+using _17_02_2025.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _17_02_2025.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
