141eea90f9558251630296a8c35fec2c71001fc8
halawardeh <halawardeh12102001@gmail.com>
2025-02-24 18:04:46 +0300

    اخيرا

---
+++ 24_02_2025/24_02_2025/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _24_02_2025.Models;
+
+public partial class Department
+{
+    public int Id { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? PhoneNumber { get; set; }
+}
